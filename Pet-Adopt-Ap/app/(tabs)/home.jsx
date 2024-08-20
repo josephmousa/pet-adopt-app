66c6c7e4cd50ecda92727b060b2b826b5e59193d
JosephMousa_2453578m <2453578m@student.gla.ac.uk>
2024-08-20 23:46:52 +0300

    Pet category list

--- Pet-Adopt-Ap/app/(tabs)/home.jsx
+++ Pet-Adopt-Ap/app/(tabs)/home.jsx
@@ -2,6 +2,7 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import Header from '../../components/Home/Header'
 import Slider from '../../components/Home/Slider'
+import PetListByCategory from '../../components/Home/PetListByCategory'
 
 export default function Home() {
   return (
@@ -14,7 +15,7 @@ export default function Home() {
       {/* Slider */}
       <Slider></Slider>
       {/* Category */}
-
+      <PetListByCategory></PetListByCategory>
       {/* List of Pets */}
 
       {/* Add new Pet option */}
---
+++ Pet-Adopt-Ap/components/Home/Category.jsx
@@ -0,0 +1,76 @@
+import { View, Text, FlatList, Image, StyleSheet, TouchableOpacity } from 'react-native'
+import { collection, getDocs } from "firebase/firestore";
+import React, { useEffect, useState } from 'react'
+import {db} from './../../config/FirebaseConfig'
+import Colors from './../../constants/Colors'
+
+export default function Category() {
+    
+    const [categoryList, setCategoryList] = useState([]);
+    const [selectedCategory, setSelectedCategory] = useState('Dogs');
+    useEffect(() => {
+        getCategories();
+    }, [])
+
+    const getCategories = async () => {
+        setCategoryList([])
+        const snapshot = await getDocs(collection(db,'Category'));
+        snapshot.forEach((doc) => {
+            setCategoryList(categoryList=>[...categoryList,doc.data()])
+        })
+
+    }
+
+  return (
+    <View style={{
+        marginTop: 20,
+    }}>
+      <Text style={{
+        fontFamily: 'outfit-medium',
+        fontSize: 20,
+      }}>Category</Text>
+
+      <FlatList data={categoryList}
+                numColumns={4}
+                renderItem={({item, index}) => (
+                    <TouchableOpacity 
+                    onPress={()=>setSelectedCategory(item.name)}
+                    style={{
+                        flex:1
+                    }}>
+                        <View style={[styles.container, selectedCategory==item.name&&styles.selectedCategoryContainer]}>
+                            <Image source={{uri:item?.imageUrl}}
+                                    style={{
+                                        width:40,
+                                        height:40,
+                                    }}>
+
+                            </Image>
+                        </View>
+                        <Text style={{
+                            textAlign:'center',
+                            fontFamily:'outfit',
+                        }}>{item?.name}</Text>
+                    </TouchableOpacity>
+                )}>
+
+      </FlatList>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    container:{
+       backgroundColor:Colors.LIGHT_PRIMARY,
+       padding:15,
+       alignItems:'center',
+       borderWidth:1,
+       borderRadius:15,
+       borderColor:Colors.PRIMARY,
+       margin:5
+    },
+    selectedCategoryContainer:{
+        backgroundColor:Colors.SECONDARY,
+        borderColor:Colors.SECONDARY,
+    }
+})
