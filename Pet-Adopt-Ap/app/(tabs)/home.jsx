22278f4bcf1dac873d85f0f80ec40d659e105e28
JosephMousa_2453578m <2453578m@student.gla.ac.uk>
2024-08-20 22:58:43 +0300

    Slider component in Home screen

--- Pet-Adopt-Ap/app/(tabs)/home.jsx
+++ Pet-Adopt-Ap/app/(tabs)/home.jsx
@@ -1,6 +1,7 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import Header from '../../components/Home/Header'
+import Slider from '../../components/Home/Slider'
 
 export default function Home() {
   return (
@@ -11,7 +12,7 @@ export default function Home() {
       {/* Header */}
       <Header></Header>
       {/* Slider */}
-
+      <Slider></Slider>
       {/* Category */}
 
       {/* List of Pets */}
